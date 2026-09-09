3a136ec47731f12d6aaff9b660eadffe1dc99ea0
ShahzaibAkhtar7 <shahzaibakhtar819@gmail.com>
2026-09-09 18:26:49 -0400

    Add CarMake and CarModel Django models, populate data, and save getallcarmakes output

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -9,6 +9,10 @@ import logging
 import json
 from django.views.decorators.csrf import csrf_exempt
 
+# Updated Imports for Car models and Populate script
+from .models import CarMake, CarModel
+from .populate import initiate
+
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
 
@@ -65,6 +69,19 @@ def registration(request):
     else:
         data = {"userName": username, "error": "Already Registered"}
         return JsonResponse(data)
+
+
+# Get Cars view (Lab Requirement)
+def get_cars(request):
+    count = CarMake.objects.filter().count()
+    print(count)
+    if(count == 0):
+        initiate()
+    car_models = CarModel.objects.select_related('car_make')
+    cars = []
+    for car_model in car_models:
+        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
+    return JsonResponse({"CarModels": cars})
 
 
 # Get Dealerships view
